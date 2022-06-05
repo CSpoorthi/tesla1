8903695f31fd6feffa161a8ae21cd8d248e4126e
C Spoorthi <cspoorthi25@gmail.com>
2022-06-05 20:08:06 +0530

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from "styled-components"
+import Section from "./Section"
+import Models from "./Models"
+import Modely from "./Modely"
+function home() {
+  return (
+    <Container>
+          <Models
+            title="Model S"
+            description="Order Online for Touchless Delievery"
+            backgroundImg="model-s.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+            />
+          <Modely
+                      title="Model Y"
+                      description="Order Online for Touchless Delievery"
+                      backgroundImg="model-y.jpg"
+                      leftBtnText="Custom Order"
+                      rightBtnText="Existing Inventory"
+            />
+      <Section
+                      title="Model 3"
+                      description="Order Online for Touchless Delievery"
+                      backgroundImg="model-3.jpg"
+                      leftBtnText="Custom Order"
+                      rightBtnText="Existing Inventory"
+            />
+                  <Section
+                        title="Model X"
+                        description="Order Online for Touchless Delievery"
+                        backgroundImg="model-x.jpg"
+                        leftBtnText="Custom Order"
+                        rightBtnText="Existing Inventory"
+              />
+       <Section
+                      title="Lowest Costing solar panel in World"
+                      description="Money Back Guranteed"
+                      backgroundImg="solar-panel.jpg"
+                      leftBtnText="Order Now"
+                      rightBtnText="Learn More"
+            />
+                <Section
+                      title="Solar Roof "
+                      description="Produce Clean Energy From Your Roof "
+                      backgroundImg="solar-roof.jpg"
+                      leftBtnText="Shop Now"
+                      rightBtnText="Learn More"
+            />
+                <Section
+                      title="Accessories"
+                      description=""
+                      backgroundImg="accessories.jpg"
+                      leftBtnText="Shop Now"
+                     
+            />
+    </Container>
+  )
+}
+
+export default home
+const Container =styled.div`
+  height: 100vh;
+  `
